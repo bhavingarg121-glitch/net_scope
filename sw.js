cae10cf2147f19236c4b99def1285f69129166b5
Bhavin Garg <bhavingarg121@gmail.com>
2026-05-31 17:22:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
+const CACHE_NAME = "netscope-cache-v1";
+
+const urlsToCache = [
+  "/"
+  "/index.html",
+  "/manifest.json",
+  "/Icon.png"
+];
+
 self.addEventListener("install", event => {
   console.log("Service Worker Installed");
   self.skipWaiting();
