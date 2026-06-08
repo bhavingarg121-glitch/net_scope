5166f8849611f8909de4d5f8b621047da4732131
Bhavin Garg <bhavingarg121@gmail.com>
2026-06-08 18:26:41 +0530

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,10 +1,13 @@
-
 export default async function handler(req, res) {
+
   if (req.method !== "POST") {
-    return res.status(405).end();
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
   }
 
-  res.status(200).json({
+  return res.status(200).json({
     success: true
   });
+
 }
