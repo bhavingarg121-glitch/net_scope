87f496976961e9ea15ce4ba137ca43fb68f0e7cd
Bhavin Garg <bhavingarg121@gmail.com>
2026-06-08 18:27:22 +0530

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,10 +1,14 @@
 export default function handler(req, res) {
+
   const size = 20 * 1024 * 1024;
+
   const buffer = Buffer.alloc(size);
 
-  res.setHeader("Content-Type", "application/octet-stream");
-  res.setHeader("Content-Length", size);
-  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+  res.setHeader(
+    "Content-Type",
+    "application/octet-stream"
+  );
 
   res.send(buffer);
+
 }
