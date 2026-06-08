528e98da23c7dd6450b5822c56d1f0aade875c8d
Bhavin Garg <bhavingarg121@gmail.com>
2026-06-08 17:26:31 +0530

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,10 +1,10 @@
-export default async function handler(
-  req,
-  res
-) {
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
 
   res.status(200).json({
-    success:true
+    success: true
   });
-
 }
